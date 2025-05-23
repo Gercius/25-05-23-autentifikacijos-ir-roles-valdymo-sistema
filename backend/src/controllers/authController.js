3dc45a6b07cd46f46898ccab1e8b8e3c517743ee
gercius <gerciokaz@gmail.com>
2025-05-23 19:38:37 +0300

    fix login bug

--- backend/src/controllers/authController.js
+++ backend/src/controllers/authController.js
@@ -36,7 +36,7 @@ export const register = async (req, res, next) => {
     }
 };
 
-export const login = async (req, res) => {
+export const login = async (req, res, next) => {
     const { email, password } = req.body;
     try {
         const user = await prisma.user.findUnique({ where: { email } });
