f3081994e7803946af1e45305f63fc8c1cbdb1c2
gercius <gerciokaz@gmail.com>
2025-05-23 20:25:23 +0300

    add not found page, initial routes

--- frontend/src/components/App.jsx
+++ frontend/src/components/App.jsx
@@ -1,10 +1,22 @@
+import { Route, Routes } from "react-router-dom";
 import Header from "./Header";
+import Dashboard from "../pages/Dashboard";
+import Login from "../pages/Login";
+import Register from "../pages/Register";
+import NotFound from "../pages/NotFound";
 
 function App() {
     return (
         <div className="container-root">
             <Header />
-            <main>main</main>
+            <main>
+                <Routes>
+                    <Route path="/" element={<Dashboard />} />
+                    <Route path="/login" element={<Login />} />
+                    <Route path="/register" element={<Register />} />
+                    <Route path="*" element={<NotFound />} />
+                </Routes>
+            </main>
         </div>
     );
 }
