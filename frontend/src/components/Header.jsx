4f34659da13792605e6cf220e26b44d9b6706133
gercius <gerciokaz@gmail.com>
2025-05-24 00:00:19 +0300

    add header

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,18 +1,51 @@
+import { useState } from "react";
 import { Link } from "react-router-dom";
+import closeMenuIcon from "../assets/close-menu-icon.svg";
+import openMenuIcon from "../assets/open-menu-icon.svg";
 
 const Header = () => {
+    const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+    const handleMenu = () => {
+        setIsMenuOpen((prev) => setIsMenuOpen(!prev));
+    };
+
     return (
         <header>
-            <span>logo</span>
-            <nav>
-                <ul>
-                    <li>
-                        <Link to="/">Prisijungti</Link>
-                    </li>
-                    <li>Registruotis</li>
-                    <li>Atsijungti</li>
-                </ul>
-            </nav>
+            <div className="logo-burger-menu-wrapper">
+                <span>logo</span>
+                <button className="mobile-menu-btn" onClick={handleMenu}>
+                    {isMenuOpen ? <img src={closeMenuIcon} alt="" /> : <img src={openMenuIcon} alt="" />}
+                </button>
+                <nav className="menu-desktop">
+                    <ul>
+                        <li>
+                            <Link to="/login">Prisijungti</Link>
+                        </li>
+                        <li>
+                            <Link to="/register">Registruotis</Link>
+                        </li>
+                        {/* <li>
+                            <Link to="/">Atsijungti</Link>
+                        </li> */}
+                    </ul>
+                </nav>
+            </div>
+            {isMenuOpen && (
+                <nav className="menu-mobile">
+                    <ul>
+                        <li onClick={handleMenu}>
+                            <Link to="/login">Prisijungti</Link>
+                        </li>
+                        <li onClick={handleMenu}>
+                            <Link to="/register">Registruotis</Link>
+                        </li>
+                        {/* <li>
+                            <Link to="/">Atsijungti</Link>
+                        </li> */}
+                    </ul>
+                </nav>
+            )}
         </header>
     );
 };
