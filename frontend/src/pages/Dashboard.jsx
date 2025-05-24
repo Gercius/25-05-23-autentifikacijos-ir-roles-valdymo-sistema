6ca08e4c9cfce46582d813974413dd2719ba63f7
gercius <gerciokaz@gmail.com>
2025-05-24 14:08:59 +0300

    show appropriate message for authenticated user

--- frontend/src/pages/Dashboard.jsx
+++ frontend/src/pages/Dashboard.jsx
@@ -2,8 +2,9 @@ import { useAuth } from "../context/AuthContext";
 
 const Dashboard = () => {
     const { userRoles } = useAuth();
+    const isAdmin = userRoles.includes("admin");
 
-    return <div>{userRoles.includes("admin") ? <p>Veri nais, uzeikit</p> : <p>Ate</p>}</div>;
+    return <div>{isAdmin ? <p>Sveiki admin</p> : <p>Sveiki {userRoles[0]}</p>}</div>;
 };
 
 export default Dashboard;
